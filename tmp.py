af779b6ccb5361c8eaa36269ce4107afd96ff2db
jfioasd <120231014+jfioasd@users.noreply.github.com>
2023-08-20 15:19:47 +0800

    Create tmp.py

---
+++ tmp.py
@@ -0,0 +1,26 @@
+def z(x):
+    return 0
+
+def s(x):
+    return x+1
+
+def k(x):
+    return lambda *n: n[x-1]
+
+def P(g, h):
+    def t(*x):
+        tmp = g(*x[:-1])
+        for i in range(x[-1]):
+            tmp = h(*x[:-1], i, tmp)
+        return tmp
+    return t
+
+def C(g, *h):
+    return lambda *x: g(*[i(*x)for i in h])
+
+def M(g):
+    x = 0
+    while 1:
+        if g(x) == 0:
+            return x
+        x += 1
